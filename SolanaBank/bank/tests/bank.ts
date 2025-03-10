ba6dc7a060be644e2ce5000a8d79d818e16694d6
Alexi Jordanov <alexiworld@yahoo.com>
2025-03-10 12:30:38 -0700

    Fix anchor tests, it was missing ts-mocha and ts-mode to run successfully. Replaced one depracated method with its successor.

--- SolanaBank/bank/tests/bank.ts
+++ SolanaBank/bank/tests/bank.ts
@@ -1,29 +1,28 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Bank } from "../target/types/bank";
-import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
-import { assert } from "chai";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Bank } from '../target/types/bank';
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
+import { assert } from 'chai';
 
-describe("bank", () => {
+describe('bank', () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   let connection = anchor.getProvider().connection;
   const program = anchor.workspace.Bank as Program<Bank>;
 
   const user = Keypair.generate();
 
-  before("prepare", async () => {
+  before('prepare', async () => {
     await airdrop(connection, user.publicKey);
   });
 
-  it("Is initialized!", async () => {
-
-    const [bankPda, bump] = await PublicKey.findProgramAddress(
-      [Buffer.from("bankaccount"), user.publicKey.toBuffer()],
+  it('Is initialized!', async () => {
+    const [bankPda, bump] = await PublicKey.findProgramAddressSync(
+      [Buffer.from('bankaccount'), user.publicKey.toBuffer()],
       program.programId
     );
 
     await program.methods
-      .create("Bank Title")
+      .create('Bank Title')
       .accounts({
         bank: bankPda,
         user: user.publicKey,
@@ -32,13 +31,13 @@ describe("bank", () => {
       .signers([user])
       .rpc();
 
-    console.log("Bank account created successfully:", bankPda.toBase58());
+    console.log('Bank account created successfully:', bankPda.toBase58());
   });
 });
 
 async function airdrop(connection: any, address: any, amount = 1000000000) {
   await connection.confirmTransaction(
     await connection.requestAirdrop(address, amount),
-    "confirmed"
+    'confirmed'
   );
 }
